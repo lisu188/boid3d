62aaf9c18adbe5aca13ce0018893f39cb045c875
Lis, Andrzej <andrzej.lis3@gmail.com>
2022-06-18 16:01:04 +0200

    second commit

--- dodge_the_creeps_3d/Main.gd
+++ dodge_the_creeps_3d/Main.gd
@@ -1,14 +1,22 @@
 extends Node
 
-export(PackedScene) var mob_scene
-
+export(PackedScene) var player_scene
 
 func _ready():
 	randomize()
-
-
+	
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
 		# warning-ignore:return_value_discarded
 		get_tree().reload_current_scene()
-
+ 
+func _on_PlayerTimer_timeout():
+	for i in 10:
+		var player=player_scene.instance()
+		var player_spawn_location = get_node("Player")
+		var vector=Vector3()
+		vector.x=i
+		vector.y=i
+		vector.z=i
+		player.transform.origin =Vector3.ZERO + vector
+		add_child(player)
--- dodge_the_creeps_3d/Player.gd
+++ dodge_the_creeps_3d/Player.gd
@@ -1,42 +1,20 @@
 extends KinematicBody
 
 # How fast the player moves in meters per second.
-export var speed = 14
+export var speed = 32
 
 var velocity = Vector3.ZERO
 
+func _ready():
+	velocity.x=(randf()-0.5)*speed
+	velocity.y=(randf()-0.5)*speed
+	velocity.z=(randf()-0.5)*speed
+	
 func _physics_process(delta):
-	var direction = Vector3.ZERO
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_key_pressed(KEY_Z):
-		direction.y += 1
-	if Input.is_key_pressed(KEY_X):
-		direction.y -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-
-	if direction != Vector3.ZERO:
-		# In the lines below, we turn the character when moving and make the animation play faster.
-		direction = direction.normalized()
-		$Pivot.look_at(translation + direction, Vector3.ONE)
-		$AnimationPlayer.playback_speed = 4
-	else:
-		$AnimationPlayer.playback_speed = 1
-
-	velocity.x = direction.x * speed
-	velocity.y = direction.y * speed
-	velocity.z = direction.z * speed
+	$Pivot.look_at(translation + velocity.normalized(), Vector3.ONE)
 
 	velocity = move_and_slide(velocity, Vector3.UP)
 	
-	is_on_ceiling()
-	print(transform.origin)
-
 	# Here, we check if we landed on top of a mob and if so, we kill it and bounce.
 	# With move_and_slide(), Godot makes the body move sometimes multiple times in a row to
 	# smooth out the character's motion. So we have to loop over all collisions that may have
@@ -48,4 +26,3 @@ func _physics_process(delta):
 	#		var mob = collision.collider
 	#		if Vector3.UP.dot(collision.normal) > 0.1:
 	#			velocity.y = bounce_impulse
-
